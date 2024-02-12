11cd8aac2ede3566a9ca744b20b0244742302de6
DominikRousa <145441116+DominikRousa@users.noreply.github.com>
2024-02-12 14:57:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const score = 0
+
+function checkAnsw(){
+
+    score = 0
+  
+    if(document.getElementById('correct1').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct2').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct3').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct4').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct5').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct6').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct7').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct8').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct9').checked){
+      score =+ 1
+    }
+    if(document.getElementById('correct10').checked){
+      score =+ 1
+    }
+    console.log(score)
+}
